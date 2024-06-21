c81bf06d117c64fbafed4a8af8bdc913a58b94cf
Utsav1 Kumar <Utsav1.Kumar@ril.com>
2024-06-21 11:56:40 +0530

    updated

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,28 +1,17 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import CodeIcon from "@mui/icons-material/Code";
 import ReorderIcon from "@mui/icons-material/Reorder";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 import HistoryIcon from "@mui/icons-material/History";
 import AddAlertIcon from "@mui/icons-material/AddAlert";
-import { Button } from "react-bootstrap";
 
-export default function TemporaryDrawer() {
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
-
-  const DrawerList = (
-    <Box sx={{ width: 225 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
+const SideNav = () => {
+  return (
+    <div>
+      <List sx={{ ".css-10hburv-MuiTypography-root": { fontSize: "0.8rem" } }}>
         <ListItemButton>
           <CodeIcon />
           <ListItemText alignitems="center" primary={"Tests"} />
@@ -44,25 +33,8 @@ export default function TemporaryDrawer() {
           <ListItemText primary={"Alerts"} />
         </ListItemButton>
       </List>
-    </Box>
-  );
-
-  return (
-    <div>
-      <Button className="drawerbutton" onClick={toggleDrawer(true)}>
-        Open drawer
-      </Button>
-      <Drawer
-        // hideBackdrop={true}
-        open={open}
-        onClose={toggleDrawer(false)}
-        sx={{
-          "&.MuiDrawer-root .MuiDrawer-paper": { marginTop: "39px" },
-          ".css-10hburv-MuiTypography-root": { fontSize: "0.8rem" },
-        }}
-      >
-        {DrawerList}
-      </Drawer>
     </div>
   );
-}
+};
+
+export default SideNav;
